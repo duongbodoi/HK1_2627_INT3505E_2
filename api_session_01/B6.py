88bdab88f3d4eec27fd54bc7137386e5c4794b38
ADMIN <24021443@vnu.edu.vn>
2026-09-16 00:18:58 +0700

    Bài tập về nhà tuần 1

--- api_session_01/B6.py
+++ api_session_01/B6.py
@@ -7,8 +7,22 @@ def find(bid):
 # LIST — GET /books
 @app.route("/books",methods=["GET"])
 def list_books() :
-    n = int(request.args.get("limit", 100))
-    return jsonify(BOOKS[:n]), 200
+    #BTVN a) GET /books?q=...
+    books_return=BOOKS.copy()
+    q=request.args.get("q")
+    sort_by=request.args.get("sort")
+    if q:
+        books_return.clear()
+        for book in BOOKS:
+            if q.lower() in book["title"].lower():
+                books_return.append(book)
+    #b)sort theo ?sort=title
+    if sort_by =="title":
+        books_return=sorted(books_return,key=lambda x: x["title"].lower())
+    return jsonify(books_return),200
+
+
+
 
 # DETAIL — GET /books/<int:id>
 @app.route("/books/<int:id>")
@@ -18,18 +32,22 @@ def get_book(id):
         return jsonify({"error":"Sách không tồn tại"}),404
     return jsonify(book),200
 # CREATE — POST /books
-@app.route("/books",methods=["POST","PUT"])
-def create_or_update_book():
+@app.route("/books",methods=["POST"])
+def create_book():
     global _next
     body=request.get_json(silent=True) or {}
     t,a=body.get("title"),body.get("author")
-    if not t or not a :
-        return jsonify({"error":"Lỗi request r ní"}),400
-    book={"id": _next ,"title":t,"author":a}
+    year=body.get("year")
+    if not t or not a or not year:
+        return jsonify({"error":"Lỗi request r ní"}),404
+    # bắt buộc field year là số ≥ 1900
+    if not isinstance(year,int):
+        return jsonify({"error":"year bắt buộc phải là số"}),400
+    if year<=1900 :
+        return jsonify({"error":"year phải lớn hơn 1900"}),400
     _next+=1
-    if request.method=="PUT":
-        BOOKS.append(book)
-        return jsonify({"mes":"ĐÃ thêm thành công"}),200
+    book={"id": _next ,"title":t,"author":a}
+    BOOKS.append(book)
     return jsonify(book),201, {"Location":f"/books/{book['id']}"}
 # UPDATE — PUT, DELETE — DELETE (xem bên phải)
 @app.route("/books/<int:bid>", methods=["PUT", "DELETE"])
@@ -41,5 +59,6 @@ def modify_book(bid):
         return jsonify(book), 200
     BOOKS.remove(book)
     return"", 204
+
 if __name__=="__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
